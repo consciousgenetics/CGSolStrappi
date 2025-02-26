d1ab7e1406e27105e55a9c9f0e579154d803f87f
Your Name <mhal236@uky.edu>
2025-02-25 21:52:20 -0500

    Update configuration for Strapi Cloud deployment

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,9 +7,14 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: {
-        rejectUnauthorized: false,
+      ssl: env.bool('DATABASE_SSL', true) && {
+        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
       },
+      schema: env('DATABASE_SCHEMA', 'public'),
+    },
+    pool: {
+      min: 0,
+      max: 5,
     },
     debug: false,
   },
